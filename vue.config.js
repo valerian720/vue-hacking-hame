528a6b4f7e6aad1ddf5b77e73811984b7e60fd1f
Валериан Воротников <valerian7202@gmail.com>
2023-10-30 21:52:51 +0300

    build datetime + last build commit mark

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
+const { gitDescribeSync } = require("git-describe");
+//
+process.env.VUE_APP_GIT_HASH = gitDescribeSync().hash;
+process.env.VUE_APP_BUILD_DATETIME = new Date().toLocaleString();
+
+//
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-hacking-hame/" : "/",
   transpileDependencies: true,
+  // auto fix on save
+  chainWebpack: (config) => {
+    config.plugin("eslint").tap((args) => {
+      args[0].fix = true;
+      return args;
+    });
+  },
 });
