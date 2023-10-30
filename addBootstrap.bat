141dab7e5947c0ba718272f8fa1d2fcd072b8ce6
Валериан Воротников <valerian7202@gmail.com>
2023-10-30 21:49:53 +0300

    bootstrap

---
+++ addBootstrap.bat
@@ -0,0 +1,6 @@
+@REM 1) add to main.ts / main.json
+@REM import "bootstrap/dist/css/bootstrap.min.css";
+@REM import "bootstrap";
+
+@REM 2) run
+npm install --save bootstrap & npm install --save @popperjs/core
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
 createApp(App).use(store).use(router).mount("#app");
