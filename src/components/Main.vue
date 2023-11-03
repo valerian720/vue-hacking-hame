39fede06d17b3f69459a9da1cbc3460053a41c6b
Валериан Воротников <valerian7202@gmail.com>
2023-11-03 23:40:22 +0300

    mark

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,9 @@
 <template>
   <main class="container">
-    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+    <div
+      class="p-4 p-md-5 mb-4 text-white rounded bg-dark"
+      style="height: 50em"
+    >
       <div class="col-md-6 px-0">
         <h1 class="display-4 fst-italic">
           Title of a longer featured blog post
